04571d82bf9f02b13a14ed06a6c6cdf7f5ec9537
u2i-h1 <5h1r0h1u2i@gmail.com>
2020-11-26 16:41:43 +0900

    add emulator_function.py

---
+++ emu/emulator_function.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import emulator
+
+def get_code8(emu, index):
+    code = emu.memory[(emu.eip + index) & 0xffffffff]
+    if not type(code) == int:
+        code = int.from_bytes(code, 'little')
+    return code
+
+def get_sign_code8(emu, index):
+    code = emu.memory[(emu.eip + index) & 0xffffffff]
+    code = int.from_bytes(code, 'little')
+    return code & 0xff
+
+def get_code32(emu, index):
+    ret = 0
+    for i in range(4):
+        ret |= get_code8(emu, index + i) << (i * 8)
+    return ret
+
+def get_sign_code32(emu, index):
+    return get_code32(index)
+
+def get_register32(emu, index):
+    reg_name = registers_name[index]
+    return emu.registers[reg_name]
+
+def set_register32(emu, index, value):
+    reg_name = registers_name[index]
+    emu.registers[reg_name] = value
+
+def set_memory8(emu, address, value):
+    emu.memory[address] = value & 0xff
+
+def set_memory32(emu, address, value):
+    for i in range(4):
+        set_memory8(emu, address + i, value >> (i * 8))
+
+def get_memory8(emu, address):
+    return emu.memory[address]
+
+def get_memory32(emu, address):
+    ret = 0
+    for i in range(4):
+        ret |= get_memory8(emu, address + i) << (8 * i)
+    return ret
