8938030fc73c5e3a33066f5de3ddd441a67e145f
u2i <pv3f3wii@s.okayama-u.ac.jp>
2020-12-17 19:06:19 +0900

    update sample_x86.py

--- unicorn/sample_x86.py
+++ unicorn/sample_x86.py
@@ -5,6 +5,7 @@
 from __future__ import print_function # python2.7 ?
 from unicorn import *
 from unicorn.x86_const import *
+from capstone import *
 
 # エミュレーション対象の機械語
 X86_CODE32 = b"\x41\x4a" # INC ecx; DEC edx
@@ -17,8 +18,8 @@ def hook_block(uc, address, size, user_data):
     print(">>> Tracing basic block at 0x%x, block size = 0x%x" %(address, size))
 
 # 各命令に対するコールバック
-def hook_code(uc, address, size, user_data):
-    print(">>> Tracing instruction at 0x%x, instruction size = %u" %(address, size))
+# def hook_code(uc, address, size, user_data):
+#     print(">>> Tracing instruction at 0x%x, instruction size = %u" %(address, size))
 
 # x86 32bitのコードをエミュレーション
 def test_i386():
@@ -53,6 +54,22 @@ def test_i386():
         print("")
     except UcError as e:
         print("ERROR: %s" % e)
+
+
+class SimpleEngine:
+    def __init__(self):
+        self.capmd = Cs(CS_ARCH_X86, CS_MODE_32) # アーキテクチャ指定
+    def disas_single(self, data):
+        for i in self.capmd.disasm(data, 16): # 逆アセンブル
+            print("\t%s\t%s" % (i.mnemonic, i.op_str))
+            break
+disasm = SimpleEngine()
+
+def hook_code(uc, address, size, user_data):
+    print(">>> Tracing instruction at 0x%x, instruction size = %u, " %(address, size), end="")
+    # メモリから実行される命令を読む
+    ins = uc.mem_read(address, size)
+    disasm.disas_single(ins)
 
 if __name__ == '__main__':
     test_i386()
